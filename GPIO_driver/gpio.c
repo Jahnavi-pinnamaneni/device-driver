2851a05c9b613c1736f79faa185a11118b229852
Jahnavi-pinnamaneni <dolly.pinnamaneni@gmail.com>
2022-04-07 16:33:20 -0600

    Change the gpio_driver code

--- GPIO_driver/gpio.c
+++ GPIO_driver/gpio.c
@@ -1,177 +1,191 @@
-#include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kdev_t.h>
+#include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/delay.h>
 #include <linux/uaccess.h>
 #include <linux/gpio.h>
 
-#define GPIO_21 (21)
-
+/* Meta Information */
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jahnavi");
-MODULE_DESCRIPTION("GPIO Driver module");
+MODULE_DESCRIPTION("A simple gpio driver for setting a LED and reading a button");
 
-dev_t dev = 0;
-static struct  class *dev_class;
-static struct cdev etx_cdev;
+/* Variables for device and device class */
+static dev_t my_device_nr;
+static struct class *my_class;
+static struct cdev my_device;
 
-static int __init etx_driver_init(void)
-static void __exit etx_driver_exit(void)
+#define DRIVER_NAME "my_gpio_driver"
+#define DRIVER_CLASS "MyModuleClass"
 
-static int etx_open(struct inode *inode, struct file *file);
-static int etx_release(struct inode *inode, struct file *file);
-static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t * off);
-static ssize_t etx_write(struct file *filpl, const char *buf, size_t len, loff_t * off);
+/**
+ * @brief Read data out of the buffer
+ */
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+	char tmp[3] = " \n";
 
-static struct file_operations fops = 
-{
-    .owner = THIS_MODULE,
-    .read  = etx_read,
-    .write = etx_write,
-    .open  = etx_open,
-    .release = etx_release,
+	/* Get amount of data to copy */
+	to_copy = min(count, sizeof(tmp));
+
+	/* Read value of button */
+	printk("Value of button: %d\n", gpio_get_value(17));
+	tmp[0] = gpio_get_value(17) + '0';
+
+	/* Copy data to user */
+	not_copied = copy_to_user(user_buffer, &tmp, to_copy);
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief Write data to buffer
+ */
+static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+	char value;
+
+	/* Get amount of data to copy */
+	to_copy = min(count, sizeof(value));
+
+	/* Copy data to user */
+	not_copied = copy_from_user(&value, user_buffer, to_copy);
+
+	/* Setting the LED */
+	switch(value) {
+		case '0':
+			gpio_set_value(4, 0);
+			break;
+		case '1':
+			gpio_set_value(4, 1);
+			break;
+		default:
+			printk("Invalid Input!\n");
+			break;
+	}
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
+static int driver_open(struct inode *device_file, struct file *instance) {
+	printk("dev_nr - open was called!\n");
+	return 0;
+}
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
+static int driver_close(struct inode *device_file, struct file *instance) {
+	printk("dev_nr - close was called!\n");
+	return 0;
+}
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = driver_open,
+	.release = driver_close,
+	.read = driver_read,
+	.write = driver_write
 };
 
-static int etx_open(struct inode *inode, struct file * file)
-{
-    pr_info("Device file opened .. !!!\n");
-    return 0;
+/**
+ * @brief This function is called, when the module is loaded into the kernel
+ */
+static int __init ModuleInit(void) {
+	printk("Hello, Kernel!\n");
+
+	/* Allocate a device nr */
+	if( alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
+		printk("Device Nr. could not be allocated!\n");
+		return -1;
+	}
+	printk("read_write - Device Nr. Major: %d, Minor: %d was registered!\n", my_device_nr >> 20, my_device_nr && 0xfffff);
+
+	/* Create device class */
+	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+		printk("Device class can not e created!\n");
+		goto ClassError;
+	}
+
+	/* create device file */
+	if(device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) == NULL) {
+		printk("Can not create device file!\n");
+		goto FileError;
+	}
+
+	/* Initialize device file */
+	cdev_init(&my_device, &fops);
+
+	/* Regisering device to kernel */
+	if(cdev_add(&my_device, my_device_nr, 1) == -1) {
+		printk("Registering of device to kernel failed!\n");
+		goto AddError;
+	}
+
+	/* GPIO 4 init */
+	if(gpio_request(4, "rpi-gpio-4")) {
+		printk("Can not allocate GPIO 4\n");
+		goto AddError;
+	}
+
+	/* Set GPIO 4 direction */
+	if(gpio_direction_output(4, 0)) {
+		printk("Can not set GPIO 4 to output!\n");
+		goto Gpio4Error;
+	}
+
+	/* GPIO 17 init */
+	if(gpio_request(17, "rpi-gpio-17")) {
+		printk("Can not allocate GPIO 17\n");
+		goto Gpio4Error;
+	}
+
+	/* Set GPIO 17 direction */
+	if(gpio_direction_input(17)) {
+		printk("Can not set GPIO 17 to input!\n");
+		goto Gpio17Error;
+	}
+
+
+	return 0;
+Gpio17Error:
+	gpio_free(17);
+Gpio4Error:
+	gpio_free(4);
+AddError:
+	device_destroy(my_class, my_device_nr);
+FileError:
+	class_destroy(my_class);
+ClassError:
+	unregister_chrdev_region(my_device_nr, 1);
+	return -1;
 }
 
-static int etx_release(struct inode * inode, struct file * file)
-{
-    pr_info("Device file closed.... !!\n");
-    return 0;
+/**
+ * @brief This function is called, when the module is removed from the kernel
+ */
+static void __exit ModuleExit(void) {
+	gpio_set_value(4, 0);
+	gpio_free(17);
+	gpio_free(4);
+	cdev_del(&my_device);
+	device_destroy(my_class, my_device_nr);
+	class_destroy(my_class);
+	unregister_chrdev_region(my_device_nr, 1);
+	printk("Goodbye, Kernel\n");
 }
 
-static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
-{
-    uint8_t gpio_state = 0;
-    gpio_state = gpio_get_value(GPIO_21);
+module_init(ModuleInit);
+module_exit(ModuleExit);
 
-    len = 1;
-    if(copy_to_user(buf, &gpio_state, len) > 0)
-    {
-        pr_err("ERROR: Not all the bytes have been copied to the user\n");
-    }
-    pr_info("Read function: GPIO 21 = %d \n", gpio_state);
-
-    return 0;
-}
-
-static ssize_t etx_write(struct file * filp, const char __user *buf, size_t len, loff_t *off)
-{
-    uint8_t rec_buf[10] = {0};
-    if(copy_from_user(rec_buf, buf, len) > 0)
-    {
-        pr_err("ERROR: Not all bytes have been copied from the user \n");
-    }
-
-    pr_info("Write Fucntion : GPIO_21 Set = %c\n", rec_buf[0]);
-
-    if(rec_buf[0] == '1')
-    {
-        gpio_set_value(GPIO_21, 1);
-    }else if(rec_buf[0] == '0'){
-        gpio_set_value(GPIO_21, 0);
-    }else{
-        pr_err("Unknown command: Provide either 1 or 0\n");
-    }
-
-    return len;
-}
-
-static int __init etx_driver_init(void)
-{
-  /*Allocating Major number*/
-  if((alloc_chrdev_region(&dev, 0, 1, "etx_Dev")) <0){
-    pr_err("Cannot allocate major number\n");
-    goto r_unreg;
-  }
-  pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
- 
-  /*Creating cdev structure*/
-  cdev_init(&etx_cdev,&fops);
- 
-  /*Adding character device to the system*/
-  if((cdev_add(&etx_cdev,dev,1)) < 0){
-    pr_err("Cannot add the device to the system\n");
-    goto r_del;
-  }
- 
-  /*Creating struct class*/
-  if((dev_class = class_create(THIS_MODULE,"etx_class")) == NULL){
-    pr_err("Cannot create the struct class\n");
-    goto r_class;
-  }
- 
-  /*Creating device*/
-  if((device_create(dev_class,NULL,dev,NULL,"etx_device")) == NULL){
-    pr_err( "Cannot create the Device \n");
-    goto r_device;
-  }
-  
-  //Checking the GPIO is valid or not
-  if(gpio_is_valid(GPIO_21) == false){
-    pr_err("GPIO %d is not valid\n", GPIO_21);
-    goto r_device;
-  }
-  
-  //Requesting the GPIO
-  if(gpio_request(GPIO_21,"GPIO_21") < 0){
-    pr_err("ERROR: GPIO %d request\n", GPIO_21);
-    goto r_gpio;
-  }
-  
-  //configure the GPIO as output
-  gpio_direction_output(GPIO_21, 0);
-  
-  /* Using this call the GPIO 21 will be visible in /sys/class/gpio/
-  ** Now you can change the gpio values by using below commands also.
-  ** echo 1 > /sys/class/gpio/gpio21/value  (turn ON the LED)
-  ** echo 0 > /sys/class/gpio/gpio21/value  (turn OFF the LED)
-  ** cat /sys/class/gpio/gpio21/value  (read the value LED)
-  ** 
-  ** the second argument prevents the direction from being changed.
-  */
-  gpio_export(GPIO_21, false);
-  
-  pr_info("Device Driver Insert...Done!!!\n");
-  return 0;
- 
-    r_gpio:
-      gpio_free(GPIO_21);
-    r_device:
-      device_destroy(dev_class,dev);
-    r_class:
-      class_destroy(dev_class);
-    r_del:
-      cdev_del(&etx_cdev);
-    r_unreg:
-      unregister_chrdev_region(dev,1);
-  
-  return -1;
-}
-
-/*
-** Module exit function
-*/ 
-static void __exit etx_driver_exit(void)
-{
-  gpio_unexport(GPIO_21);
-  gpio_free(GPIO_21);
-  device_destroy(dev_class,dev);
-  class_destroy(dev_class);
-  cdev_del(&etx_cdev);
-  unregister_chrdev_region(dev, 1);
-  pr_info("Device Driver Remove...Done!!\n");
-}
- 
-module_init(etx_driver_init);
-module_exit(etx_driver_exit);
 
 
